4ab1bddbccd21321778eb2c147c22339bf17cad7
Phylliida Dev <phylliida.dev@gmail.com>
2017-08-09 14:00:24 -0600

    Update geo.js

--- js/geo.js
+++ js/geo.js
@@ -5,6 +5,10 @@ function sampleThings(avgAmount, maxAmount, v)
   return Math.min(maxAmount, Math.round(-Math.log(v)*avgAmount));
 }
 
+
+var adjectives =["afraid","ancient","angry","average","bad","big","bitter","black","blue","brave","breezy","bright","brown","calm","chatty","chilly","clever","cold","cowardly","cuddly","curly","curvy","dangerous","dry","dull","empty","evil","fast","fat","fluffy","foolish","fresh","friendly","funny","fuzzy","gentle","giant","good","great","green","grumpy","happy","hard","heavy","helpless","honest","horrible","hot","hungry","itchy","jolly","kind","lazy","light","little","loud","lovely","lucky","massive","mean","mighty","modern","moody","nasty","neat","nervous","new","nice","odd","old","orange","ordinary","perfect","pink","plastic","polite","popular","pretty","proud","purple","quick","quiet","rare","red","rotten","rude","selfish","serious","shaggy","sharp","short","shy","silent","silly","slimy","slippery","smart","smooth","soft","sour","spicy","splendid","spotty","stale","strange","strong","stupid","sweet","swift","tall","tame","tasty","tender","terrible","thin","tidy","tiny","tough","tricky","ugly","unlucky","warm","weak","wet","white","wicked","wise","witty","wonderful","yellow","young"];
+
+var animals=["ape","baboon","badger","bat","bear","bird","bobcat","bulldog","bullfrog","cat","catfish","cheetah","chicken","chipmunk","cobra","cougar","cow","crab","deer","dingo","dodo","dog","dolphin","donkey","dragon","dragonfly","duck","eagle","earwig","eel","elephant","emu","falcon","fireant","firefox","fish","fly","fox","frog","gecko","goat","goose","grasshopper","horse","hound","husky","impala","insect","jellyfish","kangaroo","ladybug","liger","lion","lionfish","lizard","mayfly","mole","monkey","moose","moth","mouse","mule","newt","octopus","otter","owl","panda","panther","parrot","penguin","pig","puma","pug","quail","rabbit","rat","rattlesnake","robin","seahorse","sheep","shrimp","skunk","sloth","snail","snake","squid","starfish","stingray","swan","termite","tiger","treefrog","turkey","turtle","vampirebat","walrus","warthog","wasp","wolverine","wombat","yak","zebra"];
   
 function clampLatLon(lat, lon)
 {
@@ -102,7 +106,7 @@ function generatePoints(lat, lon, seed)
       var curY = goodMod(lonCell+yOffset, lonMod);
       
       var noiseSeed = (noise.simplex2(curX, curY)+1.0)/2.0;
-      var arng = new alea("" + Math.round(noiseSeed));
+      var arng = new alea("" + Math.round(noiseSeed*100));
      
       
       var ptCountNoise = arng();
@@ -122,15 +126,14 @@ function generatePoints(lat, lon, seed)
           var actualPtX = (latCellOriginal+xOffset+ptX)/scaleFactor-90.0;
           var actualPtY = (lonCellOriginal+yOffset+ptY)/scaleFactor-180.0;
           
-          points.push([clampLatLon(actualPtX, actualPtY), Math.floor(arng()*1000.0) + ""]);
+          var animal = animals[Math.floor(arng()*animals.length-0.0000001)];
+          var adjective = adjectives[Math.floor(arng()*adjectives.length-0.0000001)];
+          
+          
+          points.push([clampLatLon(actualPtX, actualPtY), animal+ " " + adjective);
         }
       }
     }
   }
   return points;
 }  
-  
-
- 
-  
-    
