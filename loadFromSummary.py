767c5ef2a99ac4bbbed7df346ac0722e0aef9203
Phylliida Dev <phylliida.dev@gmail.com>
2025-07-03 19:02:54 -0700

    some more

---
+++ loadFromSummary.py
@@ -0,0 +1,28 @@
+import umapbrowser
+import numpy as np
+import umap
+def getSummaryData():
+    import json
+    with open("modelwelfare/mergedbailnoswap/summary.json", "rb") as f:
+        summary = json.load(f)['models']
+    
+    modelLabels = []
+    modelVectors = []
+    # sort by model name so consistent order
+    for model, modelData in sorted(list(summary.items()), key=lambda x: x[0]):
+        modelLabels.append(model)
+        curVec = np.array(modelData['rawBailPrArr'])
+        mag = np.linalg.norm(curVec, ord=2)
+        if mag == 0: mag = 1
+        curVec /= mag
+        modelVectors.append([x for x in curVec])
+    modelVectors = np.array(modelVectors)
+    umapModel = umap.UMAP(n_components=2, unique=True, verbose=True, metric='cosine')
+    umapPoints = umapModel.fit_transform(modelVectors)
+    return modelLabels, umapPoints
+
+if __name__ == "__main__":
+    labels, points = getSummaryData()
+    outputDirectory = ""
+    pageRoot = "/modelwelfare/refusalvsbailv3/umap" 
+    umapbrowser.run(dataLabels=labels, data2DPoints=points, outputDirectory=outputDirectory, pageRoot=pageRoot, webui=False)
